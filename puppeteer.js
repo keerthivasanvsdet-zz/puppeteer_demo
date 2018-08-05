b3c566d75eafc6906708a4f7559c7dbadf090b0a
Keerthivasan V <keerthi.balaji812@gmail.com>
2018-08-05 12:57:40 +0530

    Updated puppeteer.js
    Added a different use case.

--- puppeteer.js
+++ puppeteer.js
@@ -1,20 +1,29 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.goto('https://www.cinema.guru/baahubali');
-  await page.setViewport({width: 1440, height: 900});
-  await page.screenshot({path: 'screenshot.png'});
-  await page.evaluate(() => console.log(`url is ${location.href}`));
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto('https://www.google.co.in');
+    await page.setViewport({width: 1440, height: 900});
+    await page.screenshot({path: 'home_page.png'});
+    await page.evaluate(() => console.log(`url is ${location.href}`));
 
-  const actorPageSelector='#roles > div.f78847 > div > div._0b407b > div > a:nth-child(2)';
-  await page.waitForSelector(actorPageSelector);
-  await page.click(actorPageSelector);
+    await page.type('#lst-ib', 'puppeteer');
+    await page.screenshot({path: 'search_input.png'});
 
-  const resultsSelector = '#biography';
-  await page.waitForSelector(resultsSelector);
+    await page.keyboard.press('Enter');
 
-  await page.screenshot({path: 'screenshots/actor_page.png'}); 
-  await browser.close();
+    await page.waitForNavigation();
+
+    const searchImageSelector='#logo > img';
+    await page.waitForSelector(searchImageSelector);
+
+    const searchResultSelector='#rso > div:nth-child(1) > div > div:nth-child(1) > div > div > h3 > a'
+    await page.waitForSelector(searchResultSelector);
+
+    const pageTitle = await page.title();
+    console.log('Page Title: '+pageTitle);
+
+    await page.screenshot({path: 'search_results_page.png'});
+    await browser.close();
 })();
